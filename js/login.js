c8c57b5bc343960d6dadf503abfe892dea12a1d4
Tanya Prakash <jhatanya16aug@gmail.com>
2020-12-26 19:49:35 +0530

    minor

--- js/login.js
+++ js/login.js
@@ -10,77 +10,58 @@ signUpButton.addEventListener('click', () => {
 signInButton.addEventListener('click', () => {
 	container.classList.remove("right-panel-active");
 });
-//  function userlogin()
-// {
-// const url="http://127.0.0.1:8000/api/login";
 
-// var request = new XMLHttpRequest()
-
-// request.open('POST', url, true)
-// request.onload = function () {
-//   // Begin accessing JSON data here
-//   var data = JSON.parse(this.response)
-
-//   if (request.status >= 200 && request.status < 400) {
-//     data.forEach((user) => {
-//       console.log(user.email)
-//     })
-//   } else {
-//     console.log('error')
-//   }
-// }
-// request.send()
-// }
-
-// fetch(url, { 
-      
-//   // Adding method type 
-//   method: "POST", 
-    
-//   // Adding body or contents to send 
-//   body: JSON.stringify({  
-//       body:email,password, 
-//   }), 
-    
-//   // Adding headers to the request 
-//   headers: { 
-//       "Content-type": "application/json; charset=UTF-8"
-//   } 
-// }) 
-
-// // Converting to JSON 
-// .then(response => response.json()) 
-
-// // Displaying results to console 
-// .then(json => console.log(json),
-// function(e)
-// {
-// if(body.email===e.target.email && body.password === e.target.password)
-// {
-//   // redirect to dashboard
-//   window.open();
-// }
-// }); 
-
-
-function login(email,password) {
+function userlogin() {
+  const email=document.getElementById("email-login").value;
+  const password=document.getElementById("password-login").value;
+  const data={email,password};
     const url = "http://127.0.0.1:8000/api/login";
     fetch(url, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json',
       },
-      body: getFormBody({email, password }),
-      headers: { 
-        "Content-type": "application/json; charset=UTF-8"
-    } 
-  }) 
-  
+      body:JSON.stringify(data), 
+  })
   // Converting to JSON 
   .then(response => response.json()) 
-  
   // Displaying results to console 
   .then(json => console.log(json),
-    // redirect to dashboard
-    window.open());
+  // function(json){
+  // if(response >=200 && response<400)
+  //   {
+  // window.location.replace("http://127.0.0.1:5500/index.html");
+  // }
+  // else
+  // {
+  //   window.alert("error");
+  // }
+  )
+  .catch(err => console.log('Request Failed', err));
   }
+  // const myform=document.getElementById("myFormsignup")
+  function usersignup() {
+    // e.preventDefault();
+    const formData=new formData(this);
+    const email=document.getElementById("signup-email").value;
+    const password=document.getElementById("signup-password").value;
+    const username=document.getElementById("signup-user").value;
+    const data={email,password,username};
+      const url = "http://127.0.0.1:8000/api/login";
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body:JSON.stringify(data), 
+      } )
+    
+    // Converting to JSON 
+    .then(response => response.json()) 
+    
+    // Displaying results to console 
+    .then(json => console.log(json),
+    // window.location.replace("http://127.0.0.1:5500/index.html")
+    )
+    .catch(err => console.log('Request Failed', err));
+    }
